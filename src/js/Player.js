01770daab385e1df9b6f0deb148834b3b046075e
Ahmad Saeed <asaeed@gmail.com>
2023-05-15 15:48:48 -0400

    center animation after stop moving

--- src/js/Player.js
+++ src/js/Player.js
@@ -3,6 +3,8 @@ import Character from './Character'
 export default class Player extends Character {
   constructor(layer, imagePath, x, y) {
     super(layer, imagePath, x, y)
+
+    this.isCentering = false
   }
 
   update(input, map, stage) {
@@ -33,17 +35,18 @@ export default class Player extends Character {
       if (map.isVacant(newX, this.sprite.attrs.y)) this.sprite.x(newX)
     }
 
-    if (press.up || press.down || press.left || press.right) {
-      this.sprite.animation('walk')
-    } else {
-      this.sprite.animation('idle')
-    }
-
     // move map and items on it to follow player
     const xThreshDistance = 100
     const yThreshDistance = 50
     const xFromCenter = stage.width() / 2 - this.sprite.attrs.x + 32
     const yFromCenter = stage.height() / 2 - this.sprite.attrs.y - 32
+
+    if (press.up || press.down || press.left || press.right) {
+      this.sprite.animation('walk')
+    } else {
+      this.sprite.animation('idle')
+      this.centerCamera(xFromCenter, yFromCenter, map)
+    }
 
     // if user has strayed more than threshold and animation is not already running
     if (Math.abs(xFromCenter) > xThreshDistance) {
@@ -57,22 +60,24 @@ export default class Player extends Character {
       map.imageGroup.move({ x: 0, y: delta })
       this.sprite.move({ x: 0, y: delta })
     }
+  }
 
-    // if (Math.abs(yFromCenter) > yThreshDistance && !this.yAnimRunning) {
-    //   const velocity = 250 * (yFromCenter < 0 ? -1 : 1)
-    //   const anim = new Konva.Animation((frame) => {
-    //     let dist = velocity * (frame.timeDiff / 1000)
-    //     map.imageGroup.move({ x: 0, y: dist })
-    //     this.sprite.move({ x: 0, y: dist })
-    //   })
+  centerCamera(xFromCenter, yFromCenter, map) {
+    if ((Math.abs(yFromCenter) > 10 || Math.abs(xFromCenter) > 10) && !this.isCentering) {
+      const anim = new Konva.Animation((frame) => {
+        let xDist = xFromCenter * (frame.timeDiff / 1000)
+        let yDist = yFromCenter * (frame.timeDiff / 1000)
+        map.imageGroup.move({ x: xDist, y: yDist })
+        this.sprite.move({ x: xDist, y: yDist })
+      })
 
-    //   anim.start()
-    //   this.yAnimRunning = true
+      anim.start()
+      this.isCentering = true
 
-    //   setTimeout(() => {
-    //     anim.stop()
-    //     this.yAnimRunning = false
-    //   }, 800)
-    // }
+      setTimeout(() => {
+        anim.stop()
+        this.isCentering = false
+      }, 1000)
+    }
   }
 }
